1e8ab97c431093c4c623ba4edc546a84b3529378
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-09 23:53:32 +0600

    variable done

--- variable.js
+++ variable.js
@@ -1,4 +1,18 @@
-var name = 'Mahadi Hasan';
+// Numerical Variable
 var age = 23;
 var temperatures = 37;
-console.log(name, age, temperatures);
+var price = 7080;
+console.log(age, temperatures, price);
+
+// String type Variable 
+
+var Name = "Mahadi Hasan";
+var SchoolName = "Borochape M.L High School";
+var FatherName = "Abul Kalam Azad";
+
+// Boolean Type Variable
+
+var SchoolEGecks = true;
+
+var proco = false;
+
