e7bbf824ebb0ea373f44bda426abd9cfa01e03b5
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-11 02:29:05 +0600

    operation ShortHand Done

---
+++ math-shortHand-operation.js
@@ -0,0 +1,47 @@
+// recap and MathOperation 
+var ChelatePrice = 390;
+var bookPrice = 200;
+var sum = ChelatePrice + bookPrice;
+console.log(sum);
+var Subtraction = ChelatePrice - bookPrice;
+console.log(Subtraction);
+var MultiplicationPrice = ChelatePrice * bookPrice;
+console.log(MultiplicationPrice);
+var divisionPrice = ChelatePrice / bookPrice;
+console.log(divisionPrice);
+
+// ShortHand Operation 
+
+var price1 = 30;
+var price2 = 59;
+
+// sum ShortHand 
+
+// var totalPrice = price1 + price2;
+// var price1 = price1 + 4;
+// price1 = price1 + 4;
+// price1 += 4; 
+price1++;
+console.log(price1)
+
+// subtraction ShortHand 
+
+// var price1 = price1 - price2;
+// price1 = price1 - 1;
+// price1 -= 1;
+price1--;
+console.log(price1);
+
+// Multiplication  ShortHand 
+
+// var price1 = price1 * 3;
+// price1 = price1 * 3
+price1 *= 3
+console.log(price1)
+
+// Division ShortHand 
+
+// var price1 = price1 / price2;
+// price1 = price1 / price2 ;
+price1 /= 8;
+console.log(price1);
