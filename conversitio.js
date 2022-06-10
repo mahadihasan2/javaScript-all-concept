bc0af8cdcd4cf8a3b1097a1593e07bf745a6c3c7
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-11 03:00:50 +0600

    conversation done

---
+++ conversitio.js
@@ -0,0 +1,29 @@
+var firstName = "Mahadi";
+var lastName = "Hasan";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Number of String 
+
+var onionPrice = "67";
+var applePrice = "76";
+var totalPrice = onionPrice + applePrice;
+console.log(totalPrice);
+
+// conversation of String Number
+
+var mangoPrice = "679";
+var kacciPrice = "654";
+var mangoPriceNumber = parseInt(mangoPrice);
+var kacciPriceNumber = parseInt(kacciPrice);
+var totalPrice = mangoPriceNumber + kacciPriceNumber;
+console.log(totalPrice);
+
+// conversation of string float 
+
+var lichuPrice = "450.76";
+var kollaPrice = "389.76";
+var lichuPriceFloat = parseFloat(lichuPrice);
+var kollaPriceFloat = parseFloat(kollaPrice);
+var totalPriceFloat = lichuPriceFloat + kollaPriceFloat;
+console.log(totalPriceFloat);
