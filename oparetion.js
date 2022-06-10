e29d2f9f796be3c6ffb31b85f542eddc1b05ac71
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-11 01:58:51 +0600

    operation done

---
+++ oparetion.js
@@ -0,0 +1,27 @@
+// Addition Operation 
+
+var onionPrice = 300;
+var eggPrice = 100;
+var totalPrice = onionPrice + eggPrice;
+console.log("TotalAdditionPrice" + " = " + totalPrice);
+
+// Subtraction ba Differences Operation 
+
+var orangePrice = 500;
+var bannerPrice = 400;
+var DifferencesInTotal = orangePrice - bannerPrice;
+console.log("TotalDifferencePrice" + " = " + DifferencesInTotal);
+
+// Multiplication Operation 
+
+var oilPrice = 400;
+var cdPrice = 120;
+var totalMultiplicationPrice = oilPrice * cdPrice;
+console.log("TotalMultiplicationPrice" + " = " + totalMultiplicationPrice);
+
+// Divided Operation 
+
+var totalMango = 60;
+var oneMangoPrice = 10;
+var mangoQuantity = totalMango / oneMangoPrice;
+console.log("Mango Quantity" + "= " + mangoQuantity);
