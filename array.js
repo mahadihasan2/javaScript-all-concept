7981158d0fce9747e719a080f360ab575d8a6951
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-12 02:27:50 +0600

    array-index and array change of element

---
+++ array.js
@@ -0,0 +1,5 @@
+var name = ['Mahadi Hasan', 'Sujon', 'Tanvir', 'Shohag', 'Ruman'];
+console.log(name);
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+vowels = vowels.length;
+console.log('vowels length of = ', vowels)
---
+++ arrayIndex.js
@@ -0,0 +1,12 @@
+var name = ['Kobit', 'Kobita', 'Hablu', 'jamal', 'Kibriya'];
+var index = name[3];
+
+// Array Elements Check 
+var numbers = [56, 43, 78, 89, 90, 65];
+console.log(numbers)
+numbers[0] = 120;
+var secondNumber = numbers.indexOf(78);
+// var secondNumber = numbers.indexOf(708);
+console.log(numbers);
+console.log(secondNumber);
+console.log(index);
