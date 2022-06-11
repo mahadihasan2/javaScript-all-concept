eeaf1a8f81fe81197d30c2561f18e50551e55f14
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-12 01:15:49 +0600

    special edition done

--- math-shortHand-operation.js
+++ math-shortHand-operation.js
@@ -45,3 +45,17 @@ console.log(price1)
 // price1 = price1 / price2 ;
 price1 /= 8;
 console.log(price1);
+
+// Special Edition 
+
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+total = total.toFixed(3);
+total = parseFloat(total);
+console.log(total);
+
+var firstNumber1 = '0.1';
+var secondNumber2 = '0.2'
+var totalPrice = parseFloat(firstNumber1 + secondNumber2);
+console.log(totalPrice);
