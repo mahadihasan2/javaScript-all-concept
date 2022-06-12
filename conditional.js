13e0dda4ceefdddbc9377974f8a10574192b6a77
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-13 01:09:59 +0600

    condition done

---
+++ conditional.js
@@ -0,0 +1,24 @@
+var phonePrice = 50000;
+var myBaguet = 400000;
+if (phonePrice < myBaguet) {
+    console.log('Ami phone kine niya asbo');
+} else {
+    console.log('phone pore kinbo');
+}
+
+var programmingHeroCoursePrice = 5500;
+var myMoney = 4000;
+if (programmingHeroCoursePrice != myMoney) {
+    console.log('Course ta ami kinte Parbo inssallah');
+}
+else {
+    console.log('course ta ami complete korte parbo inssalllah');
+}
+
+var myProgress = 99;
+var yourProgress = 90;
+if (myProgress > yourProgress) {
+    console.log('you are a already success');
+} else {
+    console.log('iam not a perfect');
+}
