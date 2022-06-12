b51a6a1cd8a85e21236f6d72de1db72b6cc4fd84
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-13 01:25:43 +0600

    multipule condition done

---
+++ multipule-condition.js
@@ -0,0 +1,13 @@
+var getJob = true;
+var moneySaved = 500000;
+var hasFlat = false;
+if (getJob == true && moneySaved > 400000 && hasFlat == false) {
+    console.log('cholo biya koira felli !!!')
+} else {
+    console.log('tor kopale biya nai');
+}
+// if (getJob == true && moneySaved > 400000) {
+//     console.log('cholo biya koira felli !!!')
+// } else {
+//     console.log('tor kopale biya nai');
+// }
