dc6ab5eee98552cebd6631406658b81c06f70145
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-12 10:45:42 +0600

    push-pop-unshift-shift done

---
+++ push-pop.js
@@ -0,0 +1,31 @@
+// Last adding element of an array 
+
+var collegeFriends = ['kalam', 'balam', 'jalam', 'gelam'];
+console.log(collegeFriends);
+collegeFriends.push('tanvir')
+collegeFriends.push('monir')
+console.log(collegeFriends)
+
+// Last remove element from of an array 
+
+var friendsAge = [20, 14, 21, 90];
+console.log(friendsAge);
+friendsAge.pop();
+friendsAge.pop();
+friendsAge.pop();
+console.log(friendsAge);
+
+// First adding  Element from of an array 
+
+var fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+console.log(fruits);
+fruits.unshift('Lemon', 'Pineapple');
+console.log(fruits);
+
+// First remove Element from of an array 
+
+var friendsName = ['Hamid', 'Hasnat', 'Tahir', 'Soikot'];
+console.log(friendsName);
+friendsName.shift();
+friendsName.shift();
+console.log(friendsName);
