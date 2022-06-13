7289c2330b634e57322edc2d3e808f66a32ff8b6
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-14 02:20:20 +0600

    forLoop done

---
+++ Loop/forLoop.js
@@ -0,0 +1,25 @@
+// while loop er chepta version hocce forLoop 
+
+var i = 0;
+while (i <= 60) {
+    console.log('Mama tumi sob odd number daw')
+    i = i + 2;
+}
+
+
+for (rostGiven = 0; rostGiven <= 9; rostGiven++) {
+    console.log('Mama tumi amake roosden, Ami rost khabo');
+    console.log(rostGiven);
+}
+
+// odd number Using ForLoop ;
+for (i = 1; i < 60; i += 2) {
+    // var i = i + 2;
+    console.log(i, "Agula sob odd number");
+}
+
+// even number using ForLoop 
+
+for (i = 0; i < 60; i += 2) {
+    console.log(i, 'Agula sob even Number')
+}
