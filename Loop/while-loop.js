c0efdfcfb4b6f46875551187cf2242a94abfa050
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-13 13:39:49 +0600

    while loop done

---
+++ Loop/while-loop.js
@@ -0,0 +1,36 @@
+var takaDen = 0;
+// while(condition){
+//     variableName++;
+//     console.log()
+// }
+
+while (takaDen < 10) {
+    // console.log('taka paici');
+    takaDen++;
+    // console.log(takaDen)
+
+}
+
+var number = 1;
+while (number < 20) {
+    console.log(number);
+    number++;
+
+}
+
+// odd Number(bijur sonkha) 
+var number = 1;
+while (number < 30) {
+    console.log(number, 'This is odd number');
+    number = number + 2;
+}
+
+// Even Number 
+
+var number = 0;
+while (number < 30) {
+    console.log(number, "This is Even Number");
+    number = number + 2;
+}
+
+
