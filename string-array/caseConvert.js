df5afffa623a0399fcc7cd80b3f1561a1f6520dc
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-05 02:23:10 +0600

    stringConvert-done

---
+++ string-array/caseConvert.js
@@ -0,0 +1,12 @@
+// const gan = "amar sonar bangla ami tumai valobasi";
+// const propose = "I LOVE YOU";
+// // console.log(propose.toLocaleLowerCase());
+// // console.log(gan.toLocaleLowerCase());
+
+const userInput = "Mahadi";
+const userName = (userInput.toLocaleLowerCase())
+const loveName = "mahadi";
+const thisName = (loveName.toLocaleLowerCase())
+if (userName == thisName) {
+    console.log("user exists");
+};
