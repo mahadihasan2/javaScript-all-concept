ff0bbe3805484080d35f57c58c3a0304e3b3f105
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-14 11:58:40 +0600

    fundamentail concept

---
+++ es-6/recap-old.js
@@ -0,0 +1,61 @@
+// Variable 
+var condition = true;
+// let condition = false;
+const confidence = true;
+// console.log(condition);
+// console.log(confidence);
+
+// Condition 
+let basaVara = 6000;
+if (basaVara > 6000) {
+    console.log("BasaVara is very big");
+} if (basaVara < 6000) {
+    console.log("Bara nite parbo na")
+} if (basaVara == 6000) {
+    console.log("bara nite parbo");
+} if (basaVara != 6000) {
+    console.log("Ai basa valo na")
+} if (basaVara >= 6000) {
+    console.log("Nite pari tobe condition ace");
+} if (basaVara <= 6000) {
+    console.log("Ai basata temon valo hoobe na");
+
+} else {
+    console.log("Sob e bujlam tobe pore  kotha volbo")
+}
+
+// Array Declare in JavaScript
+const numbers = [5, 65, 837, 98, 43, 32];
+numbers.push();
+numbers.pop();
+numbers.indexOf[1];
+numbers[0] = 888;
+console.log(numbers)
+
+// Function 
+
+function fullName(first, second) {
+    const name = first + " " + second
+    return name;
+}
+const personName = fullName("Rahim", "Korim");
+console.log(personName);
+
+
+// Loop 
+//    While And For Loop
+let number = 0;
+while (number < 20) {
+    console.log(number)
+    number++;
+
+
+}
+
+for (let i = 0; i <= 40; i++) {
+    console.log(i);
+}
+
+// Object in JavaScript 
+const bottle = { color: "Red", contain: "Water", wight: "10kg", height: "40kg" };
+console.log(bottle);
