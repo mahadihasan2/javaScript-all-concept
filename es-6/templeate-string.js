220e41edd1f9e252446627f1253053360997ca6f
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-15 09:02:28 +0600

    template string is done

---
+++ es-6/templeate-string.js
@@ -0,0 +1,21 @@
+const multiline = 'This is the Bangladesh \n' +
+    'it is my Country \n' + 'My country is very beautiful \n' + 'Because it land is similar land';
+// console.log(multiline);
+
+const lineBreak = `My name is Mahadi Hasan Rimon
+My country name is Bangladesh
+My favorite food is chiken masala
+My Distris name is Narsingdi.
+`
+// console.log(lineBreak);
+
+// Dynamic using by BackTrick 
+const first = "Mahadi";
+const last = "Hasan";
+const fullName = `${first} ${last}`;
+console.log(fullName);
+
+const count = 4;
+const friends = ['Rakib', 'Mahim', 'Kalam', 'Mahadi']
+const friend = `<h3> my friend name is:${first} ${last}. and friend count is:${count}. this friends is: ${friends}. Has Money is: ${friends.length * 500} <h3>`;
+console.log(friend)
