3d665115700147e64768b0080f70c70f24570e51
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-16 13:23:46 +0600

    dobule number done

---
+++ more-es6/map.js
@@ -0,0 +1,7 @@
+const numbers = [5, 7, 9, 11];
+const double = numbers.map(x => x * 2);
+console.log(double);
+
+// 2nd way
+// const dobuleNumber = number => number * 2;
+// console.log(dobuleNumber(numbers))
