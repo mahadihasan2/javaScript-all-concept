389de68fac78f1a7243295ae5f3e7f75c2c072d9
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-20 13:26:08 +0600

    load data for website

--- ApiIntro/api/api.js
+++ ApiIntro/api/api.js
@@ -10,3 +10,19 @@ function loadData() {
         .then(response => response.json())
         .then(data => console.log(data))
 }
+
+function loadUsers() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(data => displayUser(data));
+}
+function loadPost() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+function displayUser(data) {
+    console.log(data);
+
+}
