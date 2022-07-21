ae87d3b8789bb00a25883fa7bd0473851747cbd0
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-22 00:59:47 +0600

    display post done

---
+++ ApiIntro/api/post.js
@@ -0,0 +1,22 @@
+function loadPost() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(data => displayPost(data))
+}
+loadPost();
+function displayPost(posts) {
+    const postContainer = document.getElementById("posts");
+    for (const post of posts) {
+        // console.log(post);
+        const div = document.createElement('div');
+        div.innerHTML = `<h3>${post.title} </h3>
+        <p>${post.body} </p>
+        `;
+        postContainer.appendChild(div);
+
+    };
+
+
+
+}
+// loadPost()
