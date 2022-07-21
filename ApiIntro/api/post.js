705f0c67c522e96c44b384027d00f5d41f5c6d5e
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-22 01:10:35 +0600

    update post data

--- ApiIntro/api/post.js
+++ ApiIntro/api/post.js
@@ -9,6 +9,7 @@ function displayPost(posts) {
     for (const post of posts) {
         // console.log(post);
         const div = document.createElement('div');
+        div.classList.add("post");
         div.innerHTML = `<h3>${post.title} </h3>
         <p>${post.body} </p>
         `;
