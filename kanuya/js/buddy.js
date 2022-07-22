4a91436aacebb3919df8ff1b7ee72fffa993acda
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-22 16:51:33 +0600

    buddy data load and displayShoe done

---
+++ kanuya/js/buddy.js
@@ -0,0 +1,18 @@
+const loadDataOfBuddy = () => {
+    fetch("https://randomuser.me/api/?results=5")
+        .then(res => res.json())
+        .then(data => dispalayDataOfBuddy(data));
+}
+loadDataOfBuddy();
+
+const dispalayDataOfBuddy = data => {
+    const buddies = data.results;
+    console.log(buddies);
+    const divElementBuddies = document.getElementById("buddies");
+    for (const buddy of buddies) {
+        console.log(buddy.email);
+        const p = document.createElement('p');
+        p.innerText = `Name:${buddy.name.first} ${buddy.name.last} ${buddy.name.title} and Email:${buddy.email}`;
+        divElementBuddies.appendChild(p);
+    }
+}
