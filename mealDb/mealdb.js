e2c590c2c4a7589a4adb3784f7054afaf8f21469
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-22 23:49:59 +0600

    meal db input field data load

---
+++ mealDb/mealdb.js
@@ -0,0 +1,19 @@
+const searchFood = () => {
+    const searchField = document.getElementById("search-field");
+    const searchText = searchField.value;
+    console.log(searchText);
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}
+    `
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.meals));
+
+
+
+    searchField.value = "";
+}
+
+const displayMealDb = meals => {
+
+}
