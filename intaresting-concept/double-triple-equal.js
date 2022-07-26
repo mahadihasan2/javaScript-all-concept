86677f7cc13b8b4ce784d23e0b58858bf7676475
mahadihasan2 <mahadihasan125643@gmail.com>
2022-07-27 02:29:41 +0600

    double and triple is done

---
+++ intaresting-concept/double-triple-equal.js
@@ -0,0 +1,65 @@
+
+// Check the Number 
+const first = 2;
+const second = 2;
+if (first == second) {
+    console.log("This Value is True");
+
+} else {
+    console.log("This Value is False");
+}
+
+// Check the type for double Equal  
+const a = "2";
+const b = 2;
+if (a == b) {
+    console.log("A is a true");
+
+} else {
+    console.log("B is a false");
+}
+
+// Check the Number and boolean for Double Equal 
+const x = "2";
+const y = true;
+if (x == y) {
+    console.log("This Value is True");
+} else {
+    console.log("This Value is False");
+}
+
+// or o = false and 1 = true 
+const f = "1";
+const g = true;
+if (f == g) {
+    console.log("this Value is True");
+} else {
+    console.log("This Value is False")
+}
+
+// Triple Equal work is (Check the number and data type)
+const n = "4";
+const t = 4;
+if (n === t) {
+    console.log("this is the true Value")
+} else {
+    console.log("this is the false value")
+}
+
+// More Comparison 
+const d = { name: "Mahadi Hasan" };
+const j = { name: "Mahadi Hasan" };
+if (d == j) {
+    console.log("There are same");
+} else {
+    console.log("There are is not Same");
+}
+
+// Array Check 
+const r = [];
+const i = [];
+if (n == i) {
+    console.log("There are Same");
+} else {
+    console.log("There are is not Same");
+}
